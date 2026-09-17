7ef48b06bb78ae0c33bb5253a3040b9433eff6e6
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-09-17 10:43:46 +0530

    feat: initialize mini ticket system with Express and PostgreSQL - Create database configuration file to manage PostgreSQL connection - Set up server with Express and establish database connection on startup

---
+++ src/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const pool = require("./config/db");
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+const startServer = async () => {
+  try {
+    await pool.query("SELECT NOW()");
+    app.listen(PORT, () => {
+      console.log(`Server is running on port: ${PORT}`);
+      console.log("Database is connected");
+    });
+  } catch (error) {
+    console.error("❌ Database connection failed:", error.message);
+    process.exit(1);
+  }
+};
+
+startServer();
