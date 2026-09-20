5de2e99fd9f00e77ef2b53b417daec75922c71b2
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-09-20 09:22:42 +0530

    feat: implement create ticket api

--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -1,10 +1,38 @@
 const express = require("express");
+const pool = require("../config/db");
+
 const router = express.Router();
 
+// Test route
 router.get("/", (req, res) => {
   res.json({
     message: "Ticket API is working",
   });
+});
+
+// Create ticket
+router.post("/", async (req, res) => {
+  try {
+    const { title, description, priority } = req.body;
+
+    const result = await pool.query(
+      `INSERT INTO tickets (title, description, priority)
+       VALUES ($1, $2, $3)
+       RETURNING *`,
+      [title, description, priority || "medium"],
+    );
+
+    res.status(201).json({
+      message: "Ticket created successfully",
+      ticket: result.rows[0],
+    });
+  } catch (error) {
+    console.error("❌ Error creating ticket:", error.message);
+
+    res.status(500).json({
+      message: "Failed to create ticket",
+    });
+  }
 });
 
 module.exports = router;
--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const ticketRoutes = require("./routes/ticket.routes");
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json());
 app.use("/api/tickets", ticketRoutes);
 
 const startServer = async () => {
